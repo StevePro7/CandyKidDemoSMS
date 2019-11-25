a59486cf29143d3c9741fcf0a4c2582fd285fb25
StevePro7 <steveprozune@hotmail.com>
2019-11-25 20:41:07 +0000

    no message

--- dev/build.bat
+++ dev/build.bat
@@ -22,15 +22,15 @@ set /a _started=_hours*60*60*100+_min*60*100+_sec*100+_cs
 ::sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 _snd_manager.c
 ::cd ..
 
-cd engine
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 asm_manager.c
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 content_manager.c
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 font_manager.c
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 input_manager.c
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 player_manager.c
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 screen_manager.c
-sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 tile_manager.c
-cd ..
+REM cd engine
+REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 asm_manager.c
+REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 content_manager.c
+REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 font_manager.c
+REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 input_manager.c
+REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 player_manager.c
+REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 screen_manager.c
+REM sdcc -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 tile_manager.c
+REM cd ..
 
 sdcc -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
 
@@ -50,13 +50,8 @@ sdcc -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC0
 ..\crt0\crt0_sms.rel main.rel ^
 ..\lib\SMSlib.lib ^
 ..\lib\PSGlib.rel ^
-engine\asm_manager.rel ^
-engine\content_manager.rel ^
-engine\font_manager.rel ^
-engine\input_manager.rel ^
-engine\player_manager.rel ^
-engine\screen_manager.rel ^
-engine\tile_manager.rel ^
+devkit\_sms_manager.rel ^
+devkit\_snd_manager.rel ^
 gfx.rel ^
 psg.rel
 
@@ -89,4 +84,4 @@ if exist "*.sym" del "*.sym" > nul
 
 :: Run
 ::java -jar C:\SEGA\Emulicious\Emulicious.jar output.sms
-::output.sms
+output.sms
--- dev/main.c
+++ dev/main.c
@@ -12,22 +12,24 @@ void main( void )
 	static bool global_pause;
 	//unsigned char open_screen_type;
 
-	/*devkit_SMS_init();
+	devkit_SMS_init();
 	devkit_SMS_displayOff();
 
-	devkit_SMS_setSpriteMode( devkit_SPRITEMODE_NORMAL() );
+	devkit_SMS_setSpritePaletteColor( 0, 3, 0, 0 );
+
+	/*devkit_SMS_setSpriteMode( devkit_SPRITEMODE_NORMAL() );
 	devkit_SMS_useFirstHalfTilesforSprites_False();
 	devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );
 
 	engine_asm_manager_clear_VRAM();
 	engine_content_manager_load_tiles();
 	engine_content_manager_load_sprites();
-	engine_screen_manager_init();
+	engine_screen_manager_init();*/
 
-	devkit_SMS_displayOn();*/
+	devkit_SMS_displayOn();
 
 	for( ;; )
 	{
-		//devkit_SMS_waitForVBlank();
+		devkit_SMS_waitForVBlank();
 	}
 }
