5375612aa40c4e51ee21c745d4bc8433c993e9f6
StevePro7 <steveprozune@hotmail.com>
2019-11-25 22:05:02 +0000

    no message

--- dev/main.c
+++ dev/main.c
@@ -6,9 +6,9 @@ void main( void )
 	static bool global_pause;
 	unsigned char open_screen_type;
 
-	engine_asm_manager_clear_VRAM();
 	devkit_SMS_init();
 	devkit_SMS_displayOff();
+	engine_asm_manager_clear_VRAM();
 
 	devkit_SMS_setSpriteMode( devkit_SPRITEMODE_NORMAL() );
 	devkit_SMS_useFirstHalfTilesforSprites_False();
--- dev/screen/title_screen.c
+++ dev/screen/title_screen.c
@@ -1,15 +1,13 @@
 #include "title_screen.h"
 #include "..\devkit\_sms_manager.h"
-#include "..\engine\global_manager.h"
-//#include "..\engine\locale_manager.h"
-#include "..\engine\hack_manager.h"
+#include "..\engine\asm_manager.h"
 #include "..\engine\content_manager.h"
 #include "..\engine\enum_manager.h"
-#include "..\engine\font_manager.h"
 
 void screen_title_screen_load()
 {
 	devkit_SMS_displayOff();
+	engine_asm_manager_clear_VRAM();
 	engine_content_manager_load_tiles();
 	engine_content_manager_load_sprites();
 	devkit_SMS_displayOn();
@@ -17,6 +15,5 @@ void screen_title_screen_load()
 
 void screen_title_screen_update( unsigned char *screen_type )
 {
-	
 	*screen_type = screen_type_title;
 }
