0c6308f7a71e00f55b06b573143f67052c83a3d0
StevePro7 <steveprozune@hotmail.com>
2019-11-26 11:49:31 +0000

    no message

--- dev/gfx.h
+++ dev/gfx.h
@@ -20,7 +20,7 @@ extern const unsigned char	sprites__palette__bin[];
 #define				sprites__palette__bin_size 16
 
 extern const unsigned char	sprites__tiles__psgcompr[];
-#define				sprites__tiles__psgcompr_size 62
+#define				sprites__tiles__psgcompr_size 950
 
 extern const unsigned char	tree_avoid__palette__bin[];
 #define				tree_avoid__palette__bin_size 16
