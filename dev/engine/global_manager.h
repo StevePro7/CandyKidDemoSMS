7e647ff4555ad498ad8fa0c17aedfb12a995ab25
StevePro7 <steveprozune@hotmail.com>
2019-11-26 12:03:23 +0000

    no message

--- dev/engine/global_manager.h
+++ dev/engine/global_manager.h
@@ -3,29 +3,29 @@
 
 #define MAX_SCREEENS		5
 
-// Content.
-#define MUSIC_PSG			music_psg
-#define SOUND1_PSG			sound1_psg
-#define SOUND2_PSG			sound2_psg
-#define SOUND3_PSG			sound3_psg
-
-// Sprites.
-#define SPRITE_TILES		80
-
-// Steps.
-#define GAMER_MAX_STEPS		8
-#define GAMER_STD_STEPS		1
-
-// Delay.
-#define SPLASH_DELAY		150
-#define ENEMY_STD_DELAY		75
-#define ENEMY_MIN_DELAY		25
+//// Content.
+//#define MUSIC_PSG			music_psg
+//#define SOUND1_PSG			sound1_psg
+//#define SOUND2_PSG			sound2_psg
+//#define SOUND3_PSG			sound3_psg
+//
+//// Sprites.
+//#define SPRITE_TILES		80
+//
+//// Steps.
+//#define GAMER_MAX_STEPS		8
+//#define GAMER_STD_STEPS		1
+//
+//// Delay.
+//#define SPLASH_DELAY		150
+//#define ENEMY_STD_DELAY		75
+//#define ENEMY_MIN_DELAY		25
 
 // Paths.
 #define GAMER_MAX_FRAME		56
 #define GAMER_MAX_PATHS		8
 
 // Sounds
-#define SOUNDS_MAX			3
+//#define SOUNDS_MAX			3
 
 #endif//_GLOBAL_MANAGER_H_
